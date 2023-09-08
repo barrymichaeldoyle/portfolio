89e51014a9da29b1507476a4f354d502f2ead04e
Barry Michael Doyle <barry.doyle@universal.one>
2023-09-08 19:04:56 +0200

    initial layout complete

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Contact() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World (home page)
+      Hello World (contact page)
     </main>
   );
 }
